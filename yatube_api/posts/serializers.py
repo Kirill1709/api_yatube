8662273a22389ac65ac8d5e5dc7e582343dea7e3
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-06-21 17:49:29 +0300

    Sprint 9. For second review 2. Delete get_text

--- yatube_api/posts/serializers.py
+++ yatube_api/posts/serializers.py
@@ -8,14 +8,10 @@ class PostSerializer(serializers.ModelSerializer):
         slug_field='username',
         read_only=True,
         default=serializers.CurrentUserDefault())
-    text = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = ('id', 'text', 'author', 'image', 'pub_date')
-
-    def get_text(self, obj):
-        return obj.text[:100]
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -23,12 +19,8 @@ class CommentSerializer(serializers.ModelSerializer):
         slug_field='username',
         read_only=True,
         default=serializers.CurrentUserDefault())
-    text = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
         fields = ('id', 'author', 'post', 'text', 'created')
         read_only_fields = ('post',)
-
-    def get_text(self, obj):
-        return obj.text[:100]
